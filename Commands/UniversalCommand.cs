183cb0daf21dd6b7d021121a13c83b2102061577
Jakub Mnich <jk.mbot@gmail.com>
2021-03-06 20:31:58 +0100

    Initial commit

---
+++ Commands/UniversalCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AnaLight.Commands
+{
+    public class UniversalCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public delegate void BasicHandler();
+        
+        public BasicHandler Handler { get; }
+
+        public UniversalCommand(BasicHandler aHandler)
+        {
+            Handler = aHandler;
+            CanExecuteChanged?.Invoke(this, null);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Handler?.Invoke();
+        }
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AnaLight.Commands;
+using System.Diagnostics;
+
+namespace AnaLight
+{
+    public class MainWindowViewModel
+    {
+        public ICommand NewTabCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            NewTabCommand = new UniversalCommand(() => NewTab());
+        }
+
+        private void NewTab()
+        {
+            Debug.WriteLine("ae");
+        }
+
+    }
+}
